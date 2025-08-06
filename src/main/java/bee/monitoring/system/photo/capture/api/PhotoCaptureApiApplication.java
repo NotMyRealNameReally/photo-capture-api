ce7c8beb39ae1ab7f5ac62af1b51cf05324b5c8d
NotMyRealNameReally <cieslinski.jeremiasz@gmail.com>
2025-08-06 20:59:25 +0200

    Initial commit

---
+++ src/main/java/bee/monitoring/system/photo/capture/api/PhotoCaptureApiApplication.java
@@ -0,0 +1,13 @@
+package bee.monitoring.system.photo.capture.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoCaptureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoCaptureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bee/monitoring/system/photo/capture/api/PhotoCaptureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bee.monitoring.system.photo.capture.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoCaptureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
